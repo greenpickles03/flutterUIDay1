218846a67ea2caa0506496330f312a58839033df
greenpickles03 <71419350+greenpickles03@users.noreply.github.com>
2023-09-25 22:28:44 +0800

    flutter first commit

---
+++ android/app/src/main/kotlin/com/GreenApple/flutter_ui_day_1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.GreenApple.flutter_ui_day_1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,156 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner:false,
+    home: HomePage(),
+  ));
+  
+}
+
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(244, 243, 243, 1),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: IconButton(
+          icon: const Icon(Icons.menu, color: Colors.black87,), onPressed: () {  },
+        ),
+      ),
+      body:  SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                color: Colors.white, 
+                borderRadius: BorderRadius.vertical(
+                  bottom: Radius.circular(30),
+                )
+              ),
+              padding: const EdgeInsets.all(20),
+              child:  Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text('Find your',
+                    style: TextStyle(color: Colors.black87, fontSize: 25),
+                  ),
+                  const SizedBox(
+                    height: 5,
+                  ),
+                  const Text('Inspiration',
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontSize: 40
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      color: const Color.fromRGBO(244, 243, 243, 1,),
+                      borderRadius: BorderRadius.circular(15)
+                    ),
+                    child: const TextField(
+                      decoration: InputDecoration(
+                        border: InputBorder.none,
+                        prefixIcon: Icon(
+                          Icons.search,
+                          color: Colors.black87,
+                        ),
+                        hintText: "Search you're looking for",
+                        hintStyle: TextStyle(
+                          color: Colors.grey,
+                          fontSize: 15,
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text('Promo Today',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Container(
+                    height: 200,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: [
+                        promoCard('assets/images/one.jpg'),
+                        promoCard('assets/images/two.jpg'),
+                        promoCard('assets/images/three.jpg'),
+                        promoCard('assets/images/four.jpg'),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+
+  }
+
+  Widget promoCard(image){
+    return AspectRatio(
+      aspectRatio: 2 / 3,
+      child: Container(
+        decoration:  BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          image: DecorationImage(
+            fit: BoxFit.cover,
+            image: AssetImage(image),
+          ),
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            gradient: LinearGradient(
+                begin: Alignment.bottomRight,
+                stops: [0.1, 0.9],
+                colors: [
+                  Colors.black.withOpacity(.8),
+                  Colors.black.withOpacity(.1),
+                ]
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+}
